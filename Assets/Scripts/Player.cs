f70dd27fc5e21f7f68038e8dd00f94f1c04e6164
Yann Kiener <8420234+yannKiener@users.noreply.github.co>
2023-02-07 16:57:18 +0100

    Added space button to switch to QUANTUM WORLD WOW SUCH INCREDIBLE

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject background;
+    public Color basicBackgroundColor;
+    public Color quantumBackgroundColor;
+
+    private SpriteRenderer backgroundSpRenderer;
+    private bool isInQuantum = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        backgroundSpRenderer = background.GetComponent<SpriteRenderer>();
+        //backgroundSpRenderer.color = basicBackgroundColor;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("space"))
+        {
+            if (isInQuantum)
+            {
+                switchToBasic();
+            } else
+            {
+                switchToQuantum();
+            }
+
+        }
         
+    }
+
+    private void switchToQuantum()
+    {
+        Debug.Log("Switched to Quantum world");
+        isInQuantum = true;
+        backgroundSpRenderer.color = quantumBackgroundColor;
+    }
+
+    private void switchToBasic()
+    {
+        Debug.Log("Switched to Basic world");
+        isInQuantum = false;
+        backgroundSpRenderer.color = basicBackgroundColor;
     }
 }

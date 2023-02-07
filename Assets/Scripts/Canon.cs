ca3346ba28048bf1acdcc1db11081a13bffb8333
Yann Kiener <8420234+yannKiener@users.noreply.github.co>
2023-02-07 16:57:18 +0100

    now canon moves out !

--- Assets/Scripts/Canon.cs
+++ Assets/Scripts/Canon.cs
@@ -6,18 +6,26 @@ public class Canon : MonoBehaviour
 {
     public GameObject playerToFire;
     public Vector2 fireForce;
+    public float moveSpeed;
+    public float moveAfter;
+    public float deleteAfterMove;
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D playerRBody = playerToFire.GetComponent<Rigidbody2D>();
         playerRBody.AddForce(fireForce, ForceMode2D.Impulse);
+        Destroy(gameObject, moveAfter + deleteAfterMove);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveAfter -= Time.deltaTime;
+        if (moveAfter < 0)
+        {
+            transform.position = transform.position - new Vector3(Time.deltaTime * moveSpeed, 0, 0);
+        }
     }
 }

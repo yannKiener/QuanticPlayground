2ec00c83159a4a865eefd0a204614d17387dde9c
Yann Kiener <8420234+yannKiener@users.noreply.github.co>
2023-02-07 16:57:18 +0100

    Added first scene, sprites, scripts and a canon that fires a ball lel

---
+++ Assets/Scripts/Canon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+    public GameObject playerToFire;
+    public Vector2 fireForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D playerRBody = playerToFire.GetComponent<Rigidbody2D>();
+        playerRBody.AddForce(fireForce, ForceMode2D.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
